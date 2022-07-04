3a20984f8aca71f82a20f0b8c13ebec48273b97d
Breno Bezerra de Melo Alencar <onerb98@hotmail.com>
2022-07-04 16:00:04 -0300

    Corrige erro na sobrescrita do método calcular

--- src/main/java/CalculaPrecoProduto.java
+++ src/main/java/CalculaPrecoProduto.java
@@ -2,7 +2,7 @@ import java.math.BigDecimal;
 import java.util.List;
 
 public class CalculaPrecoProduto implements CalculaPreco{
-    BigDecimal calcular(Produto produto){
+    public BigDecimal calcular(Produto produto){
         BigDecimal preco = produto.getPreco();
         for(AlteracaoPreco alteracaoPreco:produto.getAlteracoesPossiveis()){
             preco = preco.add(alteracaoPreco.alteraPreco(preco));
