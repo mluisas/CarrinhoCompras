022879f87f27291a0c2dd6fa65c98d23629d3e5f
JoseAntonioPaiva <73199980+JoseAntonioPaiva@users.noreply.github.com>
2022-07-05 12:15:53 -0300

    Add files via upload

---
+++ src/main/java/CalculaPrecoCarrinho.java
@@ -0,0 +1,13 @@
+import java.math.BigDecimal;
+
+public class CalculaPrecoCarrinho implements CalculaPreco<Carrinho>{
+
+    @Override
+    public BigDecimal calcular(Carrinho carrinho) {
+        BigDecimal soma= BigDecimal.ZERO;
+        carrinho.getProdutos().forEach((produto, quantidade) -> {
+            soma.add(produto.getPreco().multiply(BigDecimal.valueOf(quantidade)));
+        });
+        return soma;
+    }
+}
